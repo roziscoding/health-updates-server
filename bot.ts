35c84fd49e44a824b86fd54a1e671583ca57635f
Roz <roz@rjmunhoz.me>
2025-04-08 15:24:49 -0300

    chore(updates): remove inline button

--- bot.ts
+++ bot.ts
@@ -1,28 +1,27 @@
-import { Bot, InlineKeyboard } from "grammy/mod.ts";
+import { Bot } from "grammy/mod.ts";
 
 // deno-lint-ignore no-explicit-any
 export function useBot(token: string, adminChatId: number, fetchPosts: (force: boolean) => Promise<any>) {
   const bot = new Bot(token);
 
-  bot.command("start", (ctx) => {
-    InlineKeyboard.text("CHECK");
-    ctx.reply("Click to check for updates", {
-      reply_markup: new InlineKeyboard().text("CHECK", "check"),
-    });
-  });
-
   bot
     .filter((ctx) => ctx.hasChatType("private"))
     .filter((ctx) => ctx.from.id === adminChatId)
-    .callbackQuery(
+    .command(
       "check",
       async (ctx) => {
-        await ctx.reply("Fetching...");
+        await ctx.reply("Fetching...", {
+          reply_parameters: {
+            message_id: ctx.msg.message_id,
+            allow_sending_without_reply: true,
+          },
+        });
 
         await fetchPosts(true);
 
         await ctx.reply("Done");
       },
     );
+
   return bot;
 }
